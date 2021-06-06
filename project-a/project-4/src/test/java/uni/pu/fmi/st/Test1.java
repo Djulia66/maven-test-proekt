f550195796335d4a47bb1c44fe1219b165dd3cdb
Djulia966 <juls96@abv.bg>
2021-06-06 20:18:00 +0300

    [ Bug #5442] Fix Thread sleep

--- project-a/project-4/src/test/java/uni/pu/fmi/st/Test1.java
+++ project-a/project-4/src/test/java/uni/pu/fmi/st/Test1.java
@@ -6,6 +6,7 @@ public class Test1 {
 	@Test
 	public void test() throws InterruptedException {
 		System.out.println("---Strat Test1---");
+		
 		Thread.sleep(2500);
 		
 		System.out.println("---Stop Test1---");
